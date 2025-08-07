72b0a0444e889d86decba85d5a48942e313621f5
I741356 <wen-tzu.lo@sap.com>
2025-08-07 23:29:30 +0200

    python2

--- level1/Ressources/quick_offset.py
+++ level1/Ressources/quick_offset.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import sys
+
 pattern = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4"
 hex_val = sys.argv[1].replace('0x', '') if len(sys.argv) > 1 else "37634136"
-ascii_le = bytes.fromhex(hex_val).decode('ascii', errors='ignore')
-ascii_be = bytes.fromhex(hex_val)[::-1].decode('ascii', errors='ignore')
-print(f"Hex: 0x{hex_val}")
-print(f"Little-endian: '{ascii_le}' -> offset: {pattern.find(ascii_le) if pattern.find(ascii_le) != -1 else 'Not found'}")
-print(f"Big-endian: '{ascii_be}' -> offset: {pattern.find(ascii_be) if pattern.find(ascii_be) != -1 else 'Not found'}") 
+
+# Convert hex to ASCII (little-endian and big-endian)
+ascii_le = hex_val.decode('hex')
+ascii_be = hex_val.decode('hex')[::-1]
+
+print "Hex: 0x{}".format(hex_val)
+print "Little-endian: '{}' -> offset: {}".format(ascii_le, pattern.find(ascii_le) if pattern.find(ascii_le) != -1 else 'Not found')
+print "Big-endian: '{}' -> offset: {}".format(ascii_be, pattern.find(ascii_be) if pattern.find(ascii_be) != -1 else 'Not found')
